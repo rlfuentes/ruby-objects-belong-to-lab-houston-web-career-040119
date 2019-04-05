135a100d9e619734b5ff1cae49ba9d6ed97208f1
Robert <r_lee94@yahoo.com>
2019-04-05 02:31:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,8 @@
 class Artist
-  attr_reader :name 
-  attr_accessor :artist, :title
   
-  def initialize(artist, title)
-    @artist = artist
-    @title = title
+  attr_accessor :name
   
-  end
+  
   
   
 end
